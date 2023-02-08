38f89535975dfbf4532e4a3fdfc6ccca9dc95aa0
MariaAmor8 <m.amorocho@uniandes.edu.co>
2023-02-07 22:02:27 -0500

    cambios Est-1

--- App/model.py
+++ App/model.py
@@ -75,6 +75,8 @@ def addBookTags(catalog, booktagsfile):
     Esta funcion guardar los booktags provenientes del archivo CSV.
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
+    catalog["book_tags"] = lt.newList(datastructure="ARRAY_LIST", filename=booktagsfile)
+    return catalog
     pass
 
 
@@ -102,6 +104,8 @@ def createBookTagList(catalog):
     Esta funcion crea una lista vacia para booktags.
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
+    catalog["book_tags"] = lt.newList(datastructure="ARRAY_LIST")
+    return catalog
     pass
 
 
@@ -110,6 +114,9 @@ def addBookTag(catalog, booktag):
     Esta funcion agrega un elemento a lista de booktags.
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
+    lt.addLast(catalog["book_tags"], booktag)
+    return catalog
+
     pass
 
 
